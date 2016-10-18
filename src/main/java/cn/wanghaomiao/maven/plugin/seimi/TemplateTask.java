b7dde73bff6c29b81304914ddaa2bcc4eed1d824
xiaohuo <et.tw@163.com>
2016-10-18 22:51:17 +0800

    启动脚本优化

--- src/main/java/cn/wanghaomiao/maven/plugin/seimi/TemplateTask.java
+++ src/main/java/cn/wanghaomiao/maven/plugin/seimi/TemplateTask.java
@@ -1,18 +1,11 @@
 package cn.wanghaomiao.maven.plugin.seimi;
 
 
-import freemarker.cache.ClassTemplateLoader;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateExceptionHandler;
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.logging.Log;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.HashMap;
-import java.util.Map;
+import java.net.URL;
 
 /**
  * @author github.com/zhegexiaohuozi [seimimaster@gmail.com]
@@ -20,51 +13,34 @@ import java.util.Map;
  */
 public class TemplateTask {
     private File outDir;
-    private Configuration cfg;
     private Log log;
 
     public TemplateTask(File outDir, Log log) {
         this.outDir = outDir;
         this.log = log;
-        this.cfg = new Configuration(Configuration.VERSION_2_3_22);
-        ClassTemplateLoader loader = new ClassTemplateLoader(TemplateTask.class.getClassLoader(), "template");
-        cfg.setTemplateLoader(loader);
-        cfg.setDefaultEncoding("UTF-8");
-        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
     }
 
     public void createBinFile() {
         try {
             File bin = new File(outDir, "bin");
             bin.mkdir();
-            Map<String, Object> ctx = new HashMap<String, Object>();
-
-            createFile(ctx,bin,"seimi.vmoptions");
-            createFile(ctx,bin,"seimi.cfg");
-            createFile(ctx,bin,"run.bat");
-
-            Template templateSh = cfg.getTemplate("run.sh");
-            StringWriter stringWriter = new StringWriter();
-            FileWriter shBinFile = new FileWriter(new File(bin, "run.sh"));
-            templateSh.process(ctx, stringWriter);
-            shBinFile.write(stringWriter.toString().replaceAll("\r",""));
-            shBinFile.flush();
-            shBinFile.close();
+            createFile(bin, "seimi.vmoptions");
+            createFile(bin, "seimi.cfg");
+            createFile(bin, "run.bat");
+            createFile(bin, "run.sh");
 
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
     }
 
-    public void createFile(Map<String,Object> ctx,File dir,String fileName){
+    public void createFile(File dir, String fileName) {
         try {
-            Template templateBat = cfg.getTemplate(fileName);
-            Writer binFile = new FileWriter(new File(dir, fileName));
-            templateBat.process(ctx, binFile);
-            binFile.flush();
-            binFile.close();
-        }catch (Exception e){
-            log.error(e.getMessage(),e);
+            URL resourc = this.getClass().getClassLoader().getResource("template/" + fileName);
+            assert resourc != null;
+            FileUtils.copyURLToFile(resourc, new File(dir, fileName));
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
         }
     }
 }
